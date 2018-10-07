0864da7e25e330c3a3e52ca619a036a47826e02c
SiddiquiMinhaj <43311845+SiddiquiMinhaj@users.noreply.github.com>
2018-10-07 13:55:25 +0530

    Added Stock Project Source Code

---
+++ Stock/Stock/Login.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=,;Initial Catalog=Stock;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter(@"SELECT * FROM [Stock].[dbo].[Login] where UserName ='"+textBox1.Text +"'and Password ='"+textBox2.Text +"'", con);
+            DataTable dt=new DataTable ();
+           
+            sda.Fill(dt);
+             this.Hide();
+            StockMain main = new StockMain();
+            main.Show();
+
+        }
+    }
+}
---
+++ Stock/Stock/StockMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class StockMain : Form
+    {
+
+        public StockMain()
+        {
+            InitializeComponent();
+        
+        }
+
+        private void StockMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StockMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
